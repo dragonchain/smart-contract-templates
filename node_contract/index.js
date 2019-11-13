5133cadb513641e4e5f55b8b74cc959bfdcd19a0
Dean Shelton <dean@dragonchain.com>
2019-11-12 21:02:24 -0800

    adds docs to node

--- node_contract/index.js
+++ node_contract/index.js
@@ -1,13 +1,27 @@
+/**
+ * Hi there!
+ * You most likely do not want to edit this file.
+ *
+ * This file is the entrypoint of your smart contract.
+ * The Dragonchain process which invokes this code will
+ * pass input to this executable file via STDIN.
+ *
+ * This file's sole purpose is to give that input to your
+ * SmartContract located in the ./contract/handler.js file.
+ *
+ * You probably want to edit the business logic there.
+ */
+
 const getStdin = require('get-stdin');
 const handler = require('./contract/handler');
 
 async function main() {
-  const val = await getStdin();
+  const val = await getStdin(); // <-- get input from the blockchain contract invoker
   try {
-    const res = await handler(val);
-    process.stdout.write(JSON.stringify(res));
+    const res = await handler(val); // <-- execute your smart contract
+    process.stdout.write(JSON.stringify(res)); // <-- give the output back invoker to modify any state.
   } catch (err) {
-    return console.error(err);
+    return console.error(err); // <-- log the error
   }
 }
 
